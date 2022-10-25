261b648460d924debd1f003d4149c4ea02c3de50
Magno <dimagno13@gmail.com>
2022-10-25 13:00:01 -0300

    introduction views

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
     
-    public function show(){
-        return 1;
+    public function show(User $user){
+         return view('user',compact('user')); 
     }
 }
